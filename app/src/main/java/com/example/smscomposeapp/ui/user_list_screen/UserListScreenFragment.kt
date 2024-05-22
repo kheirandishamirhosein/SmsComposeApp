cbac9bf5adcc2ded95a1d8c384488157f8b23c8a
Amirhosein Kheirandish <kheirandishamirhosein@gmail.com>
2024-05-23 02:16:33 +0330

    fix Action Button UserListScreen

--- app/src/main/java/com/example/smscomposeapp/ui/user_list_screen/UserListScreenFragment.kt
+++ app/src/main/java/com/example/smscomposeapp/ui/user_list_screen/UserListScreenFragment.kt
@@ -19,7 +19,7 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Send
+import androidx.compose.material.icons.filled.Email
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
@@ -79,8 +79,9 @@ fun UserListScreen(navController: NavController, viewModel: SmsViewModel) {
                     navController.navigate("smsUserChatScreen")
                 },
                 content = {
-                    Icon(Icons.Default.Send, contentDescription = "Send SMS")
-                }
+                    Icon(Icons.Default.Email, contentDescription = "Send SMS")
+                },
+                modifier = Modifier.padding(bottom = 46.dp, end = 16.dp)
             )
         }
     ) { padding ->
